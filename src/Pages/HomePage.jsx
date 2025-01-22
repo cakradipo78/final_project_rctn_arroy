6b4fb106364e74e26ea4bc4ec9f03fccb0cbb040
cakradipo78 <ari.heryawan@gmail.com>
2025-01-22 13:04:45 +0700

    upload003

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,45 @@
+import React, {useEffect, useState} from "react"
+import {useSelector, useDispatch} from "react-redux"
+import { apiGetMoviesNowPlaying, apiGetMoviesSearchMovie } from "../store/action"
+import CardComponent from "../ComponenPages/CardComponent"
+
+export default function HomePage(){
+
+ const dataMovies = useSelector(state => state.dataMovies)   
+ const dispatch = useDispatch()
+
+ console.log(dataMovies,"<==data dari reducer");
+ 
+
+  useEffect(() => {
+        dispatch(apiGetMoviesNowPlaying())
+
+    },[])
+
+
+     return (       
+     <>
+       <h1>MOVIE HOME PAGE</h1>
+           {/* <p>{JSON.stringify(dataMovies)}</p> */}
+       
+           <div className="row">
+           {/* <CardComponent/>  */}
+               {
+       
+                   dataMovies.map(el => (
+                   <>      
+                      
+                   <CardComponent key={el.id} id={el.id} original_title={el.original_title} popularity={el.popularity} poster_path={el.poster_path} overview={el.overview} /> 
+                  
+                   </>   
+                   ))
+               }
+           </div>
+           
+     
+     </>     
+ 
+     )
+ 
+ 
+ }
